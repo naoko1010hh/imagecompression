2af2a905eab379d33e0032f324260b108c258151
naoko1010hh <50263013+naoko1010hh@users.noreply.github.com>
2020-01-06 14:27:33 +0900

    Add files via upload

--- imagecompression.sh
+++ imagecompression.sh
@@ -6,9 +6,11 @@ do
     if [ "PNG" = "`file $1/$line | awk '{print $2}'`" ] ; then
         echo "PNGンゴ $1/$line"
         optipng $1/$line
+        cwebp -q 60 $1/$line -o $1/$line.webp 
     elif [ "JPEG" = "`file $1/$line | awk '{print $2}'`" ] ; then
         echo "JPEGンゴ $1/$line"
         jpegoptim --strip-all $1/$line
+        cwebp -q 60 $1/$line -o $1/$line.webp
     elif [ "directory" = "`file $1/$line | awk '{print $2}'`" ] ; then
         echo "フォルダを見つけたンゴ $1/$line"
         $0 $1/$line
