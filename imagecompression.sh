5b837611247fd44695886b82ab1300d0f03a1c54
naoko1010hh <50263013+naoko1010hh@users.noreply.github.com>
2020-01-06 18:08:00 +0900

    webpを生成するかしないかオプションで

--- imagecompression.sh
+++ imagecompression.sh
@@ -6,13 +6,17 @@ do
     if [ "PNG" = "`file $1/$line | awk '{print $2}'`" ] ; then
         echo "PNGンゴ $1/$line"
         optipng $1/$line
-        cwebp -q 60 $1/$line -o $1/$line.webp 
+        if [ "webp" = "$2" ] ; then
+            cwebp -q 60 $1/$line -o $1/$line.webp
+        fi
     elif [ "JPEG" = "`file $1/$line | awk '{print $2}'`" ] ; then
         echo "JPEGンゴ $1/$line"
         jpegoptim --strip-all $1/$line
-        cwebp -q 60 $1/$line -o $1/$line.webp
+        if [ "webp" = "$2" ] ; then
+            cwebp -q 60 $1/$line -o $1/$line.webp
+        fi
     elif [ "directory" = "`file $1/$line | awk '{print $2}'`" ] ; then
         echo "フォルダを見つけたンゴ $1/$line"
-        $0 $1/$line
+        $0 $1/$line $2
     fi
 done
